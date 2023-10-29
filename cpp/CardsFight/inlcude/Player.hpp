e7c8be33511d92ef1d247e70378c2d9bbbd7e9b3
tanmika <tanmika@foxmail.com>
2023-10-30 00:01:06 +0800

    update 完成BuffList

--- cpp/CardsFight/inlcude/Player.hpp
+++ cpp/CardsFight/inlcude/Player.hpp
@@ -12,6 +12,7 @@
 #include <unordered_set>
 #include <unordered_map>
 using std::vector;
+using std::pair;
 class Player;
 class BuffBase
 {
@@ -20,28 +21,123 @@ public:
 	{
 		player = p;
 	}
-	void Add(size_t);
-	void Sub(size_t);
-	size_t Sum()const;
+	void Add(size_t n)
+	{
+		num += n;
+	}
+	void Sub(size_t n)
+	{
+		num = num > n ? num - n : 0;
+	}
+	size_t Sum()const
+	{
+		return num;
+	}
 private:
 	Player* player;
+	size_t num;
 };
 
 class BuffList
 {
 public:
-	BuffList(int max_round, int buff_num)
+	BuffList(int max_round, vector<BuffBase>& buffs)
 	{
-		buffVec.resize(max_round);
-		for (auto& e : buffVec)
-			e.resize(buff_num);
-		buffSet.resize(max_round);
+		buffVec.resize(max_round +1);
+		for (auto& buff : buffs)
+			for (auto e: buffVec)
+			{
+				e = new vector<pair<BuffBase*, size_t>>;
+				e->push_back(pair<BuffBase*, size_t>(&buff, 0));
+			}
+		buffSet.resize(max_round +1);
+		for (auto e : buffSet)
+			e = new std::unordered_set<size_t>;
 	}
 	~BuffList() = default;
-	void AddBuff(size_t _id, size_t duration, size_t _time);
-	void Round();
-	void RemoveLasted();
-	void RemoveRamdon();
+	void AddBuff(size_t _id, size_t _time =1, size_t duration =0)
+	{
+		buffVec[duration]->at(_id).first->Add(_time);
+		buffVec[duration]->at(_id).second += _time;
+		if (duration == 0)return; //永久buff不登记
+		for (auto i = 0; i < _time; i++)
+		{
+			//为每层buff注册id
+			auto id = GetId();
+			//按回合数登记
+			buffSet[duration]->insert(id);
+			//按新增顺序登记
+			buffMap[id] = _id;
+		}
+	}
+	void Round()
+	{
+		//当前回合结束消失buff
+		auto ptrVec = buffVec[1];
+		auto ptrSet = buffSet[1];
+		//注销监听
+		for (auto e : *ptrSet)
+		{
+			buffMap.erase(e);
+		}
+		ptrSet->clear();
+		//消除效果
+		for (auto& e: *ptrVec)
+		{
+			e.first->Sub(e.second);
+			e.second = 0;
+		}
+		//其余buff回合数减1
+		for (auto i = 1; i < buffVec.size()-1; i++)
+		{
+			buffVec[i] = buffVec[i + 1];
+			buffSet[i] = buffSet[i + 1];
+		}
+		//最远回合数绑定
+		buffVec[buffVec.size() - 1] = ptrVec;
+		buffSet[buffSet.size() - 1] = ptrSet;
+	}
+	void RemoveLasted()
+	{
+		auto it = buffMap.begin();
+		size_t maxId = it->first;
+		for (auto iter = buffMap.begin(); iter != buffMap.end(); ++iter)
+		{
+			if (iter->first > maxId)
+			{
+				maxId = iter->first;
+				it = iter; // id最大的buff
+			}
+		}
+		for (int i = 1; i < buffSet.size(); i++)
+		{
+			if (buffSet[i]->find(maxId) != buffSet[i]->cend())
+			{
+				buffSet[i]->erase(maxId);
+				buffVec[i]->at(it->second).first->Sub(1);
+				buffVec[i]->at(it->second).second--;
+				return;
+			}
+		}
+	}
+	void RemoveRamdon()
+	{
+		auto it = buffMap.begin();
+		auto num = rand() %( buffMap.size()-1);
+		while (num--)
+			it++;
+		for (int i = 1; i < buffSet.size(); i++)
+		{
+			if (buffSet[i]->find(it->first) != buffSet[i]->cend())
+			{
+				buffSet[i]->erase(it->first);
+				buffVec[i]->at(it->second).first->Sub(1);
+				buffVec[i]->at(it->second).second--;
+				return;
+			}
+		}
+
+	}
 private:
 	size_t GetId()
 	{
@@ -49,8 +145,9 @@ private:
 		return id++;
 	}
 private:
-	vector<vector<BuffBase*>> buffVec;
-	vector<std::unordered_set<size_t>> buffSet;
+	vector<vector<pair<BuffBase*,size_t>>*> buffVec;//(回合, (buff索引id, (buff 类, 次数)))
+	vector<std::unordered_set<size_t>*> buffSet;//(回合, (本buff id))
+	std::unordered_map<size_t, size_t> buffMap;//(本buff id, buff索引id)
 };
 
 class EffectBase
